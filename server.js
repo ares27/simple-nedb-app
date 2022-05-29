a746d5678e95a4a45458a82f9c33ff82932b3da8
Byron Jehoma <jehomasb@gmail.com>
2022-05-29 13:36:41 +0200

    updated.

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const res = require("express/lib/response");
 const database = new Datastore("database.db");
 database.loadDatabase();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/greet", async function (req, res) {
   res.send({ message: "Hello World" });
 });
@@ -25,9 +28,9 @@ app.post("/api", async function (req, res) {
 });
 
 app.post("/startBot", async (req, res) => {
-  const authURL = "https://auto04.treasuryone.co.za/v1/authentication";
-  const un = "treaserauto04"; // "botc1",
-  pw = "Monday@99";
+  const authURL = "config";
+  const un = "config"; // "botc1",
+  pw = "config";
 
   try {
     const res_data = await axios.post(authURL, {
@@ -88,12 +91,93 @@ app.post("/startBot", async (req, res) => {
   }
 });
 
+app.post("/message", (req, res) => {
+  console.log(req.body);
+  let msgFrom = req.body.From;
+  let msgBody = req.body.Body;
+
+  runBot(msgBody);
+
+  res.send(`
+    <Response>
+    <Message>
+    Hello ${msgFrom}. You said: ${msgBody}
+    </Message>
+    </Response>
+  `);
+});
+
 // (function getAPIData() {
 //   console.log("jsonplaceholder...");
 //   axios
 //     .get("https://jsonplaceholder.typicode.com/todos/1")
 //     .then((response) => console.log(response.data));
 // })();
+
+async function runBot(input) {
+  const authURL = "config";
+  const un = "config"; // "botc1",
+  pw = "config";
+
+  try {
+    const res_data = await axios.post(authURL, {
+      username: un,
+      password: pw,
+    });
+    // console.log(res.data.token);
+    const token = res_data.data.token;
+
+    if (token) {
+      const resDeploy = await axios.post(
+        "https://auto04.treasuryone.co.za/v3/automations/deploy",
+        {
+          fileId: 66772, //id of the bot to execute
+          runAsUserIds: [
+            4, //id(s) of the user account to run the bot - must have default device unless specified below
+          ],
+          poolIds: [],
+          overrideDefaultDevice: false,
+          callbackInfo: {
+            url: "https://callbackserver.com/storeBotExecutionStatus", //Callback URL - not required, but can be used - can be removed if no callback needed
+            headers: {
+              "X-Authorization": token, //Callback API headers. Headers may contain authentication token, content type etc. Both key & value are of type string.
+            },
+          },
+          botInput: {
+            //optional values to map to the bot...NOTE: These values must match the exact variable names and must be defined as input values
+            sInput1: {
+              type: "STRING", //Type can be [ STRING, NUMBER, BOOLEAN, LIST, DICTIONARY, DATETIME ]
+              // string: req.body.sInput1, //key must match type, in this case string
+              string: "run bot...", //key must match type, in this case string
+            },
+            sInput2: {
+              type: "STRING",
+              string: input,
+            },
+          },
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": token,
+          },
+        }
+      );
+
+      let response = resDeploy.data;
+      console.log("deploy bot now...");
+      console.log(resDeploy.data);
+
+      res.send({
+        message: "Bot executed",
+        deploymentId: response.deploymentId,
+        body: req.body,
+      });
+    }
+  } catch (ex) {
+    console.log(ex.response);
+  }
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
